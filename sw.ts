0e15f3f6874f643b507ce658ae1ca09713817915
Alberto Padrón <alberpad@hotmail.com>
2018-11-08 13:08:23 +0000

    modificaciones de phone x

--- sw.ts
+++ sw.ts
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -42,7 +42,10 @@ self.addEventListener('activate', (e: any) => {
       if (key !== STATIC_CACHE && key.includes('static')) {
         return caches.delete(key);
       }
-      if (key !== INMUTABLE_CACHE && key.includes('inmutable')) {
+      // if (key !== INMUTABLE_CACHE && key.includes('inmutable')) {
+      //   return caches.delete(key);
+      // }
+      if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
         return caches.delete(key);
       }
     });
